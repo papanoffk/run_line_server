0b7bef924832f3221b40d69c3c17e8e14c5cad52
BurMinistr <93135399+papanoffk@users.noreply.github.com>
2023-06-14 19:30:25 +0300

    Update path to img and vid

--- testTask/run_string/views.py
+++ testTask/run_string/views.py
@@ -13,14 +13,14 @@ def index(request):
     return HttpResponse("<h2>Введите текст</h2>")
 def send_file(request):
     text = request.GET.get("text", 'Hello world!')
-    path = os.path.abspath('run_string\\static\\img\\back100.png')
-    path_to = os.path.abspath('run_string\\static\\video')
+    path = os.path.abspath('run_string/static/img/back100.png')
+    path_to = os.path.abspath('run_string/static/video')
 
     run_string_script.draw(path, text, path_to)
 
     #draw('..\\static\\img\\back100.png', text)
 
-    path_out = settings.BASE_DIR / 'run_string\\static\\video\\output.mp4'
+    path_out = settings.BASE_DIR / 'run_string/static/video/output.mp4'
 
     file = FileWrapper(open(path_out, 'rb'))
     response = HttpResponse(file, content_type='video/mp4')
